50cbcaa14670c4001629d0978941ad606316aea4
Bibek Ray <bibekray.dec@gmail.com>
2023-05-27 21:41:26 +0530

    Added files for drumkit

---
+++ index.js
@@ -0,0 +1,68 @@
+var numberOfDrumButtons = document.querySelectorAll(".drum").length;
+
+for(var i = 0; i < numberOfDrumButtons ; i++)
+{    
+    document.querySelectorAll("button")[i].addEventListener("click", handleClick);
+
+    function handleClick(){
+        var buttonInnerHTML = this.innerHTML;
+
+        makeSound(buttonInnerHTML);
+
+        buttonAnimation(buttonInnerHTML);
+    }
+    
+
+    function makeSound(key){
+
+        switch (key) {
+            case "w":
+                var audio = new Audio('sounds/tom-1.mp3');
+                audio.play();
+                break;
+            case "a":
+                var audio = new Audio('sounds/tom-2.mp3');
+                audio.play();
+                break;
+            case "s":
+                var audio = new Audio('sounds/tom-3.mp3');
+                audio.play();
+                break;
+            case "d":
+                var audio = new Audio('sounds/tom-4.mp3');
+                audio.play();
+                break;
+            case "j":
+                var audio = new Audio('sounds/snare.mp3');
+                audio.play();
+                break;
+            case "k":
+                var audio = new Audio('sounds/crash.mp3');
+                audio.play();
+                break;
+            case "l":
+                var audio = new Audio('sounds/kick-bass.mp3');
+                audio.play();
+                break;
+            default:
+                break;
+          }
+    }
+}
+
+document.addEventListener("keydown", function (event){
+    makeSound(event.key);
+
+    buttonAnimation(event.key);
+});
+
+function buttonAnimation(currentKey){
+
+    var activeButton = document.querySelector("." + currentKey);
+    
+    activeButton.classList.add("pressed");
+
+    setTimeout(function(){
+        activeButton.classList.remove("pressed");
+    }, 100);
+}
